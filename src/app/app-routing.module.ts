c5256d62cb8b1579e2a3d90d07ecd9460e8adbf5
Alejandro <tamayalejandro03@gmail.com>
2023-05-11 09:30:27 -0600

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Componentes
+import { ListUsersComponent } from './componentes/list-users/list-users.component';
+import { HomeComponent } from  './componentes/home/home.component'
+import { AddEditUserComponent } from './componentes/add-edit-user/add-edit-user.component';
+import { AllDataComponent } from './componentes/all-data/all-data.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'add', component: AddEditUserComponent },
+  { path: 'all', component: AllDataComponent },
+  { path: 'edit/:id', component: AddEditUserComponent },
+  { path: ':id', component: ListUsersComponent },
+  { path: '**', redirectTo:'', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
